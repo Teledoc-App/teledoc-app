acc77fbff24377bee5544c745cca38ada1527990
Gary Cruise <Garycruisee@gmail.com>
2023-09-26 23:30:14 +0700

    feat: users appointment history
    make a get user appointment and shows

---
+++ app/schedule/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import HistoryCards from "@/components/HistoryUserCards";
+
+export default function HistoryUserCards() {
+  const [appointments, setAppointments] = useState();
+  const URL = "http://localhost:3000/api/";
+  const token =
+    "993901663c42b1c2df10edf1674106e3d03618fcf716d975a608c38aa158f95c%7C83526fb2504952b1a4f1c4d20daf2cab3be124226ca7aaa881bdc8f3ce4983c7";
+
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  async function fetchHistory() {
+    try {
+      const response = await axios.get(URL + "users/me", config);
+      if (response.status === 200) {
+        const data = response.data;
+        setAppointments(data);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log("Error fetching appointment history:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  return <HistoryCards></HistoryCards>;
+}
+
+const colors = {
+  Pending: "#FFB74D",
+  Accepted: "#4FC3F7",
+  Done: "#81C784",
+  Rejected: "#858585",
+};
