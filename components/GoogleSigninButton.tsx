d5601530285714925d26a9fdb8244959ccd71429
meselghea <122159476+meselghea@users.noreply.github.com>
2023-09-28 20:31:53 +0700

    fix(app/login/page.tsx): fix login
    fix login google

--- components/GoogleSigninButton.tsx
+++ components/GoogleSigninButton.tsx
@@ -10,31 +10,37 @@ const GoogleSignInButton: FC<GoogleSignInButtonProps> = ({ children }) => {
   const router = useRouter();
   const userId = session?.user?.id;
   const userRole = session?.user?.role;
-  let callbackUrl = "/profile/";
-  
-  if (userRole === "patient") {
+  let callbackUrl = '/profile/';
+
+  if (userRole === 'patient') {
     callbackUrl += `patient/${userId}`;
-  } else if (userRole === "doctor") {
+  } else if (userRole === 'doctor') {
     callbackUrl += `doctor/${userId}`;
   } else {
-    console.error("Invalid user role:", userRole);
-    return;
+    console.error('Invalid user role:', userRole);
+    return null; // Mengembalikan null jika perlu
   }
-  const loginWithGoogle = async () => {
-    const signInData = await signIn('google',
-   { callbackUrl: callbackUrl,
-  });
 
-  if (signInData?.error) {
-    console.error(signInData.error);
-  } else {
-    router.push(callbackUrl);
-  }
+  const loginWithGoogle = async () => {
+    const signInData = await signIn('google', {
+      callbackUrl: callbackUrl,
+    });
+
+    if (signInData?.error) {
+      console.error(signInData.error);
+    } else {
+      router.push(callbackUrl);
+    }
+  };
+
+  return (
+    <button
+      onClick={loginWithGoogle}
+      className="bg-[#d9d9d9]/30 border border-[#d9d9d9] text-gray-500 rounded-lg w-full h-[60px] font-semibold relative"
+    >
+      {children}
+    </button>
+  );
 };
 
-
-  return(
-    <button onClick={loginWithGoogle}  className="bg-[#d9d9d9]/30 border border-[#d9d9d9] text-gray-500 rounded-lg w-full h-[60px] font-semibold relative" >{children}</button>
-  );
-}
 export default GoogleSignInButton;
