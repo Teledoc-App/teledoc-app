a697dd0caed428ea664ab403f8051f70de84944b
Gary Cruise <Garycruisee@gmail.com>
2023-09-28 22:00:45 +0700

    fix(doctor api): doctor api get by id
    get by id fix

--- app/api/doctor/[id]/route.ts
+++ app/api/doctor/[id]/route.ts
@@ -1,45 +1,63 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  try {
-    const queryParams = new URLSearchParams(req.url.split("?")[1]);
-    const doctorId = queryParams.get("doctorId");
-
-    if (!doctorId) {
-      return NextResponse.json(
-        { message: "Doctor ID is missing", success: false },
-        { status: 400 }
-      );
-    }
-
-    const doctor = await db.doctor.findUnique({
-      where: { userId: doctorId },
-      include: {
-        user: {
-          include: {
-            doctorAppointments: true,
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+  const doctor = await db.user.findUnique({
+    where: { id },
+    select: {
+      doctor: {
+        select: {
+          userId: true,
+          username: true,
+          strNumber: true,
+          price: true,
+        },
+      },
+      doctorAppointments: {
+        select: {
+          time: true,
+          date: true,
+          status: true,
+          reason: true,
+          description: true,
+          patient: {
+            select: {
+              name: true,
+              gender: true,
+              birthDate: true,
+              image: true,
+              phone: true,
+            },
           },
         },
       },
+    },
+  });
+
+  if (!doctor) {
+    let error_response = {
+      status: "fail",
+      message: "No doctor with the Provided ID Found",
+    };
+    return new NextResponse(JSON.stringify(error_response), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
     });
-
-    if (!doctor) {
-      return NextResponse.json(
-        { message: "Doctor not found", success: false },
-        { status: 404 }
-      );
-    }
-
-    return NextResponse.json({ doctor });
-  } catch (error: any) {
-    console.error(error);
-    return NextResponse.json(
-      { message: error.message, success: false },
-      { status: 500 } // Internal Server Error
-    );
   }
+
+  let json_response = {
+    status: "success",
+    data: {
+      doctor,
+    },
+  };
+  return NextResponse.json(json_response);
 }
+
 export async function PATCH(
   request: Request,
   { params }: { params: { doctorId: string } }
