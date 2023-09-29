768f4980de3ad26504c152b3c095ab7c3e8c8ae3
Gary Cruise <Garycruisee@gmail.com>
2023-09-29 20:23:02 +0700

    fix: history cards
    history cards

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -4,33 +4,38 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import HistoryCards from "@/components/HistoryUserCards";
 
-interface specialistProps {
-  id: string;
-  title: string;
-}
+// interface specialistProps {
+//   id: string;
+//   title: string;
+// }
 
-interface doctorProps {
-  doctor: {
-    username: string;
-    specialist: specialistProps;
-  };
-  image: string;
-}
-interface patientAppointment {
-  id: string;
-  specialist: specialistProps;
-  doctor: doctorProps;
-}
+// interface doctorProps {
+//   image: string;
+//   username: string;
+//   specialist: specialistProps;
+// }
+
+// interface statusProps {
+//   name: string;
+// }
+
+// interface patientAppointment {
+//   id: string;
+//   specialist: specialistProps;
+//   doctor: doctorProps;
+//   status: specialistProps;
+// }
 
 const History: React.FC = () => {
-  const [appointments, setAppointments] = useState<patientAppointment[]>([]);
+  //   const [history, setHistory] = useState<patientAppointment[]>([]);
+  const [history, setHistory] = useState();
 
   async function fetchHistory() {
     try {
       const response = await axios.get("../../api/users/me");
       if (response.status === 200) {
-        const data = response.data;
-        setAppointments(data);
+        const data = response.data[0];
+        setHistory(data);
         console.log(data);
       }
     } catch (error) {
@@ -44,20 +49,27 @@ const History: React.FC = () => {
 
   return (
     // <div className="w-full">
-    //   {appointments.map((appointment) => (
-    //     <HistoryCards
-    //       key={appointment.id}
-    //       id={appointment.id}
-    //       specialist={appointment.specialist}
-    //       doctor={{
-    //         username: appointment.doctor.doctor.username,
-    //         specialist: appointment.doctor.doctor.specialist,
-    //       }}
-    //       image={appointment.doctor.image || "default-image-url"}
-    //     />
-    //   ))}
+    //   {history ? (
+    //     history.length > 0 ? (
+    //       history.map((appointment) => (
+    //         <HistoryCards
+    //           key={appointment.id}
+    //           id={appointment.id}
+    //           specialist={appointment.specialist}
+    //           doctor={appointment.doctor}
+    //           status={appointment.status}
+    //         />
+    //       ))
+    //     ) : (
+    //       <p>No history available.</p>
+    //     )
+    //   ) : (
+    //     <p>Loading...</p>
+    //   )}
     // </div>
-    <HistoryCards></HistoryCards>
+    <div>
+      <HistoryCards></HistoryCards>
+    </div>
   );
 };
 
