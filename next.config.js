17d50d4c757d5d8b852c63ba1a1f8b1c7bedea95
meselghea <122159476+meselghea@users.noreply.github.com>
2023-10-02 19:39:10 +0700

    fix: add nextconfig enable cors vercel
    add nextconfig enable cors vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,31 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  experimental: {
+    appDir: true,
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          // https://vercel.com/guides/how-to-enable-cors
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value:
+                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            },
+          ],
+        },
+      ];
+    },
+  },
   images: {
     domains: ["img.freepik.com", "lh3.googleusercontent.com"],
   },
