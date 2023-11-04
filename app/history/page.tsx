9318a7ca6c15586b4f9e8a8f48597bde3ea1be8e
Nofrialdi <aldi.fasive@gmail.com>
2023-11-04 11:07:53 +0700

    refactor(request ekstension): popup request extension and disabele button request extension

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Dialog } from "@headlessui/react";
 import { useRouter } from "next/navigation";
 import Appointment from "../doctors/appointment/[id]/page";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { set } from "date-fns";
 
 interface Appointment {
 	id: string;
@@ -28,10 +31,31 @@ interface Appointment {
 	};
 }
 
+interface DoctorDetail {
+	userId: string;
+	username: string;
+	price: string;
+}
+
+interface Profile {
+	name: string;
+}
+
 const History: React.FC = () => {
 	const [history, setHistory] = useState<Appointment[]>();
 	const [isOpen, setIsOpen] = useState(false);
 	const router = useRouter();
+
+	const [symptoms, setSymptoms] = useState("");
+	const [description, setDescription] = useState("");
+	const [time, setTime] = useState("08:00");
+	const [username, setUsername] = useState("");
+	const [name, setName] = useState("");
+	const [gender, setGender] = useState("");
+	const [patientId, setPatientId] = useState("");
+	const [doctorId, setDoctorId] = useState("");
+	const [date, setDate] = React.useState<Date | null>(null);
+	const [statusId, setStatusId] = useState("23ba40d0-6c82-4d45-8b5c-21f8d70b959b");
 
 	async function fetchHistory() {
 		try {
@@ -39,6 +63,10 @@ const History: React.FC = () => {
 			//   console.log(response.data.data.user.patientAppointments.status);
 
 			setHistory(response.data.data.user.patientAppointments);
+			setPatientId(response.data.data.user.id);
+			setDoctorId(response.data.data.user.patientAppointments[0].doctor.doctor.userId);
+			// console.log(response.data.data.user.patientAppointments[0].doctor.doctor.userId);
+			// console.log(response);
 
 			if (response.status === 200) {
 			}
@@ -47,21 +75,159 @@ const History: React.FC = () => {
 		}
 	}
 
-	const handleExtension = (appointmentId: string) => {
-		requestExtension(appointmentId);
+	// const handleExtension = (appointmentId: string) => {
+	// 	requestExtension(appointmentId);
+
+	// 	try {
+	// 		const response = axios.post(`../../api/appointment/}`, {
+	// 			data: {
+	// 				symptoms,
+	// 				description,
+	// 				date,
+	// 				time,
+	// 				patientId,
+	// 				doctorId,
+	// 				statusId,
+	// 			},
+	// 		});
+	// 		console.log(response);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
+
+	const handleDateChange = (selectedDate: any) => {
+		setDate(selectedDate);
 	};
 
-	async function requestExtension(appointmentId: string) {
+	const arrayHours = ["08:00", "09:00"];
+
+	for (let nIndex = 10; nIndex <= 22; nIndex++) {
+		arrayHours.push(`${nIndex}:00`);
+	}
+
+	const handleTimeClick = (selectedTime: any) => {
+		setTime(selectedTime);
+	};
+
+	// async function requestExtension(appointmentId: string) {
+	// 	try {
+	// 		// const response = await axios.patch(`../../api/appointment/${appointmentId}`, {
+	// 		// 	requestExtension: true,
+	// 		// });
+	// 		const response2 = await axios.post(`../../api/appointment`, {
+	// 			data: {
+	// 				symptoms,
+	// 				description,
+	// 				date,
+	// 				time,
+	// 				patientId,
+	// 				doctorId,
+	// 				statusId,
+	// 			},
+
+	// 		});
+	// 		// console.log(response.data);
+	// 		console.log(response2);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// 	// alert("Request Extension Successful");
+	// }
+
+	const requestExtension = async (appointmentId: string) => {
 		try {
 			const response = await axios.patch(`../../api/appointment/${appointmentId}`, {
 				requestExtension: true,
 			});
-			console.log(response.data);
+			console.log(response);
 		} catch (error) {
 			console.log(error);
 		}
-		// alert("Request Extension Successful");
-	}
+	};
+
+	const handleSubmit = async (e: SyntheticEvent) => {
+		e.preventDefault();
+		router.push("../../success");
+
+		const data = {
+			symptoms,
+			description,
+			date,
+			time,
+			patientId,
+			doctorId,
+			statusId,
+		};
+		console.log(data);
+
+		var axios = require("axios");
+
+		var config = {
+			method: "post",
+			url: "../../api/appointment",
+			headers: {
+				"Cache-Control": "no-cache",
+			},
+			data: data,
+		};
+
+		axios(config)
+			.then(function (response: { data: any }) {
+				console.log(JSON.stringify(response.data));
+			})
+			.catch(function (error: any) {
+				console.log(error);
+			});
+	};
+
+	// async function addAppointment(appointmentId: string) {
+	// 	try {
+	// 		const response = await axios.post(`../../api/appointment/${appointment.}`, {
+
+	// 		})
+
+	// 		});
+	// 		console.log();
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// 	// alert("Request Extension Successful");
+	// }
+
+	// const requestExtension = async (e: SyntheticEvent) => {
+	// 	e.preventDefault();
+
+	// 	const data = {
+	// 		symptoms,
+	// 		description,
+	// 		date,
+	// 		time,
+	// 		patientId: patientId,
+	// 		doctorId: doctorId,
+	// 		statusId,
+	// 	};
+	// 	console.log(data);
+
+	// 	var axios = require("axios");
+
+	// 	var config = {
+	// 		method: "post",
+	// 		url: "../../api/appointment",
+	// 		headers: {
+	// 			"Cache-Control": "no-cache",
+	// 		},
+	// 		data: data,
+	// 	};
+
+	// 	axios(config)
+	// 		.then(function (response: { data: any }) {
+	// 			console.log(JSON.stringify(response.data));
+	// 		})
+	// 		.catch(function (error: any) {
+	// 			console.log(error);
+	// 		});
+	// };
 
 	useEffect(() => {
 		fetchHistory();
@@ -119,14 +285,105 @@ const History: React.FC = () => {
 						<div className="py-2">
 							{appointment.status.name == "done" && appointment.requestExtension == false ? (
 								<div>
-									<Link href={`/doctors/appointment/${appointment.doctor.doctor?.userId}`}>
-										<button
-											onClick={() => handleExtension(appointment.id)}
-											className="border border-[#ff5757] text-white bg-[#ff5757] text-sm  px-4 py-1 rounded-lg"
-										>
-											Request Extension
-										</button>
-									</Link>
+									{/* <Link href={`/doctors/appointment/${appointment.doctor.doctor?.userId}`}> */}
+									<button
+										onClick={() => setIsOpen(true)}
+										// onClick={() => handleExtension(appointment.id)}
+										className="border border-[#ff5757] text-white bg-[#ff5757] text-sm  px-4 py-1 rounded-lg"
+									>
+										Request Extension
+									</button>
+
+									<Dialog
+										open={isOpen}
+										onClose={() => setIsOpen(false)}
+										className={`bg-black/50 fixed top-0 left-0 w-screen h-screen flex justify-center items-center p-4`}
+									>
+										<Dialog.Panel className={`bg-white p-8 rounded-lg w-full max-w-[400px]`}>
+											<Dialog.Title className={`font-bold text-2xl`}>
+												Request Extension
+											</Dialog.Title>
+											{/* <Dialog.Description className={`mb-8`}>
+												Description for reject reason
+											</Dialog.Description> */}
+
+											{/* CHOICES */}
+											<form className="" onSubmit={handleSubmit}>
+												{/* <div className="grid mb-1">
+													<p>{appointment.doctor.doctor?.username}</p>
+													<p>{appointment.doctor.doctor?.specialist.title}</p>
+												</div> */}
+
+												<div className="grid py-1">
+													<p>Symptoms</p>
+													<input
+														className="bg-[#d9d9d9]/30 h-[30px]  px-4 rounded-lg border text-[#000000] outline-none "
+														type="text"
+														placeholder="Enter reason of appointment"
+														value={symptoms}
+														onChange={(e) => setSymptoms(e.target.value)}
+													/>
+												</div>
+
+												<div className="grid py-1">
+													<p>Descripstions</p>
+													<textarea
+														id="message"
+														value={description}
+														onChange={(e) => setDescription(e.target.value)}
+														rows={2}
+														className="block p-2.5 w-full text-m text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+														placeholder="Write your thoughts here..."
+													></textarea>
+												</div>
+
+												<div className="grid py-1">
+													<p>Date</p>
+													<LocalizationProvider dateAdapter={AdapterDayjs}>
+														<DatePicker
+															format="YYYY - MM - DD"
+															sx={{
+																width: "100%",
+																backgroundColor: "rgba(217, 217, 217, 0.3)",
+																// borderColor: "#ff5757",
+																// outline: "#ff5757",
+																borderRadius: "8px",
+															}}
+															value={date}
+															onChange={handleDateChange}
+														/>
+													</LocalizationProvider>
+												</div>
+
+												<div className="grid py-1">
+													<p>Time</p>
+													<div className="grid grid-cols-3 gap-1">
+														{arrayHours.map((sHour) => (
+															<button
+																key={`appointment-${sHour}`}
+																type="button"
+																className="bg-[#ffffff] text-[#ff5757]  border-solid border-2 border-[#ff5757] rounded-2xl hover:bg-[#ff5757] hover:text-[#ffffff]  disabled:bg-[#d9d9d9] disabled:text-[#858585] disabled:border-[#d9d9d9] focus:bg-[#ff5757] focus:text-[#ffffff]"
+																// disabled={arrayDisabledHours.includes(sHour)}
+																onClick={() => handleTimeClick(sHour)}
+															>
+																{sHour}
+															</button>
+														))}
+													</div>
+													<div className="w-full flex justify-end">
+														<button
+															className="rounded-lg mt-2 bg-[#ff5757] text-white px-4 py-2 ml-auto w-full hover:cursor-pointer"
+															onClick={() => requestExtension(appointment.id)}
+															//   disabled={true}
+														>
+															Confirm
+														</button>
+													</div>
+												</div>
+											</form>
+										</Dialog.Panel>
+									</Dialog>
+									{/* </Link> */}
 								</div>
 							) : (
 								<div>
